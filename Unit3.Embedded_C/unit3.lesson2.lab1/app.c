b14add2351452bca9d2a28b505b05c79617e6b43
NadaKaram <93673728+NadaKaram@users.noreply.github.com>
2024-03-09 18:05:39 +0200

    Delete Unit3.Embedded_C/unit3.lesson2.lab1 directory

--- Unit3.Embedded_C/unit3.lesson2.lab1/app.c
+++
@@ -1,9 +0,0 @@
-#include "uart.h"
-unsigned char buff[100]="learn_in_depth";
-unsigned char  const buff2[100]="learn_in_depth";
-unsigned char  const buff3[100]="learn_in_depth";
-void main(void)
-{
-	uart_send_string(buff);
-	
-}
